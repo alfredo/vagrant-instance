4bdfec9698bbbd9be4ff5a820cb77d3b97382a8a
Alfredo Aguirre <aguirre.alfredo07@gmail.com>
2012-01-15 22:38:38 +0000

    Version control updated

--- modules/version_control/manifests/init.pp
+++ modules/version_control/manifests/init.pp
@@ -1,5 +1,6 @@
 class version_control {
     include version_control::git
+    include version_control::hub
     include version_control::subversion
     include version_control::mercurial
     include version_control::bazaar
